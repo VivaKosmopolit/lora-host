765c39b1ff05c9506a6ed6f07473f8fa5eb71536
Daniel Harrison <danielharrison@MacBook-Pro-Daniel.local>
2020-02-14 02:34:51 +0300

    Added LogIn window

---
+++ js/script.js
@@ -0,0 +1,164 @@
+window.onscroll = () => {
+    checkOffset();
+};
+
+// LogIn modal window function
+const modal = document.querySelector('.modal-login');
+const overlay = document.querySelector('.overlay');
+
+function modalFunc() {
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+overlay.addEventListener('click', () => {
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+});
+
+const modalClose = document.querySelector('.modal-close-btn').addEventListener('click', () => {
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+});
+
+// Check window position & navigation add class function
+function checkOffset () {
+    let nav = document.querySelector('.header-top');
+    if (window.pageYOffset > 100) {
+        nav.classList.add('nav-fixed');
+    } else if (window.pageXOffset <= 990) {
+        nav.classList.remove('nav-fixed');
+    } else {
+        nav.classList.add('nav-fixed');
+        nav.style.animation = 'unset';
+    }
+}
+
+/*function checkOffset () {
+    let nav = document.querySelector('.header-top');
+    if (window.pageYOffset > 100) {
+        nav.classList.add('nav-fixed');
+    } else if (window.pageXOffset <= 990) {
+        nav.classList.add('nav-fixed');
+        nav.style.animation = 'unset';
+/!*        nav.classList.remove('nav-fixed');*!/
+    } /!*else {
+        nav.classList.add('nav-fixed');
+        nav.style.animation = 'unset';
+    }*!/
+}*/
+
+/*
+function checkOffset () {
+    let nav = document.querySelector('.header-top');
+    if (window.pageYOffset > 100) {
+        nav.classList.add('nav-fixed');
+    } else {
+        nav.classList.remove('nav-fixed');
+    }
+
+    if (window.pageXOffset <= 990) {
+        nav.classList.add('nav-fixed');
+        nav.style.animation = 'unset';
+    }
+}
+*/
+
+
+// To top button function
+$('.to-top').click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+});
+
+// Scroll reveal parameters
+ScrollReveal().reveal('.choose-item', {
+    origin: 'bottom',
+    distance: '300px',
+    interval: 200,
+    duration: 1500,
+    /*    delay: 100*/
+});
+
+// Test of value check function
+let subInp = document.getElementById('sub').value;
+let subText = document.querySelector('.sub_label');
+
+function checkValue() {
+    if (subInp.value == null) {
+        subText.innerHTML = "Zapolni pole!"
+    } else {
+
+    }
+}
+
+// Slick-slider function parameters
+$('.slick').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 920,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 620,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
+});
+
+$('.slick-header').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    arrows: false
+});
+
+// Hamburger Button function
+function btn(e) {
+    let mobNav = document.querySelector('.mobile-menu');
+    if (e.className == 'burger-wrapper') {
+        mobNav.style.display = 'block';
+        e.classList.add('burger-wrapper_active');
+    } else {
+        mobNav.style.display = 'none';
+        e.classList.remove('burger-wrapper_active');
+    }
+}
+
+// Tab function for Price section
+window.onload = function () {
+    document.querySelector('.tabs-header').addEventListener('click', fTabs);
+    function fTabs(e) {
+        if (e.target.className == 'tab-li') {
+            var dataTab = e.target.getAttribute('data-tab');
+            var tabLi = document.getElementsByClassName('tab-li');
+            for (var i = 0; i < tabLi.length; i++) {
+                tabLi[i].classList.remove('active-tab');
+            }
+            e.target.classList.add('active-tab');
+            var tabBody = document.getElementsByClassName('tab-b');
+            for (var i = 0; i < tabBody.length; i++) {
+                if (dataTab == i) {
+                    tabBody[i].style.display = 'block';
+                } else {
+                    tabBody[i].style.display = 'none';
+                }
+            }
+        }
+    }
+};
